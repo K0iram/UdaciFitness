4be35af116173513e59ae9ed0ac80a2b1cdfb5f6
mario7746 <m.kennedykavouras@gmail.com>
2018-09-22 17:35:51 -0700

    Adds Udacity Calendar component

--- components/History.js
+++ components/History.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { recieveEntries, addEntry } from '../actions'
 import { timeToString, getDailyReminderValue } from '../utils/helpers'
 import { fetchCalendarResults } from '../utils/api'
+import UdaciFitnessCalendar from 'udacifitness-calendar'
 
 
 class History extends Component {
@@ -21,11 +22,32 @@ class History extends Component {
       })
   }
 
-  render() {
+  renderItem = ({ today, ...metrics }, formattedDate, key) => (
+    <View>
+      {today ?
+        <Text>{JSON.stringify(today)}</Text>
+        :
+        <Text>{JSON.stringify(metrics)}</Text>
+      }
+    </View>
+  )
+
+  renderEmptyDate = (formattedDate) => {
     return (
       <View>
-        <Text>{JSON.stringify(this.props)}</Text>
+        <Text>No Data for this day</Text>
       </View>
+    )
+  }
+
+  render() {
+    const {entries} = this.props
+    return (
+      <UdaciFitnessCalendar
+        items={entries}
+        renderItem={this.renderItem}
+        renderEmptyDate={this.renderEmptyDate}
+      />
     )
   }
 }
