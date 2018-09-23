797452ef846237f490839ff8823c2490eb4cc1cc
mario7746 <m.kennedykavouras@gmail.com>
2018-09-23 16:04:14 -0700

    Fixes Tab Navigation bug, Adds platform switch for creatTabNavigation to switch on OS, Adds status bar to top of the app to clear the phones icons

--- App.js
+++ App.js
@@ -1,9 +1,19 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, StatusBar } from 'react-native'
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
 import TabNavigation from './components/TabNavigation'
+import { Constants } from 'expo'
+import { purple } from './utils/colors'
+
+const UdaciStatusBar = ({backgroundColor, ...props}) => {
+  return (
+    <View style={{backgroundColor, height: Constants.statusBarHeight}}>
+      <StatusBar translucent backgroundColor={backgroundColor} {...props}/>
+    </View>
+  )
+}
 
 export default class App extends React.Component {
 
@@ -11,6 +21,7 @@ export default class App extends React.Component {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{flex: 1}} >
+          <UdaciStatusBar backgroundColor={purple} barStyle='light-content'/>
           <TabNavigation/>
         </View>
       </Provider>
--- components/TabNavigation.js
+++ components/TabNavigation.js
@@ -1,49 +1,90 @@
 import React from 'react'
 import { Platform } from 'react-native'
-import { TabNavigator } from 'react-navigation/native'
+import { createBottomTabNavigator, createMaterialTopTabNavigator } from 'react-navigation'
 import { purple, white } from '../utils/colors'
 import { FontAwesome, Ionicons } from '@expo/vector-icons'
 import AddEntry from './AddEntry'
 import History from './History'
 
 
-const TabNavigation = TabNavigator({
-  History: {
-    screen: History,
+const TabNavigation =
+  Platform.OS === 'ios' ?
+    createBottomTabNavigator({
+      History: {
+        screen: History,
+        navigationOptions: {
+          tabBarLabel: 'History',
+          tabBarIcon: ({ tintColor }) =>
+            <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+        }
+      },
+      AddEntry: {
+        screen: AddEntry,
+        navigationOptions: {
+          tabBarLabel: 'Add Entry',
+          tabBarIcon: ({ tintColor }) =>
+            <FontAwesome name='plus-square' size={30} color={tintColor} />
+        }
+      }
+    }, {
+      navigationOptions: {
+        header: null
+      }
+    },
+    {
+      tabBarOptions: {
+        activeTintColor: purple,
+        style: {
+          height: 56,
+          backgroundColor: white,
+          shadowColor: 'rgba(0,0,0,0.24)',
+          shadowOffset: {
+            width: 0,
+            height: 3
+          },
+          shadowRadius: 6,
+          shadowOpacity: 1
+        }
+      }
+    })
+  :
+  createMaterialTopTabNavigator({
+    History: {
+      screen: History,
+      navigationOptions: {
+        tabBarLabel: 'History',
+        tabBarIcon: ({ tintColor }) =>
+          <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+      }
+    },
+    AddEntry: {
+      screen: AddEntry,
+      navigationOptions: {
+        tabBarLabel: 'Add Entry',
+        tabBarIcon: ({ tintColor }) =>
+          <FontAwesome name='plus-square' size={30} color={tintColor} />
+      }
+    }
+  }, {
     navigationOptions: {
-      tabBarLabel: 'History',
-      tabBarIcon: ({ tintColor }) =>
-        <Ionicons name='ios-bookmarks' size={30} color={tintColor}/>
+      header: null
     }
   },
-  AddEntry: {
-    screen: AddEntry,
-    navigationOptions: {
-      tabBarLabel: 'Add Entry',
-      tabBarIcon: ({ tintColor }) =>
-        <FontAwesome name='plus-square' size={30} color={tintColor} />
+  {
+    tabBarOptions: {
+      activeTintColor: white,
+      style: {
+        height: 56,
+        backgroundColor: purple,
+        shadowColor: 'rgba(0,0,0,0.24)',
+        shadowOffset: {
+          width: 0,
+          height: 3
+        },
+        shadowRadius: 6,
+        shadowOpacity: 1
+      }
     }
-  }
-}, {
-  navigationOptions: {
-    header: null
-  }
-},
-{
-  tabBarOptions: {
-    activeTintColor: Platform.OS === 'ios' ? purple : white,
-    style: {
-      height: 56,
-      backgroundColor: Platform.OS === 'ios' ? white : purple,
-      shadowColor: 'rgba(0,0,0,0.24)',
-      shadowOffset: {
-        width: 0,
-        height: 3
-      },
-      shadowRadius: 6,
-      shadowOpacity: 1
-    }
-  }
-})
+  })
 
 export default TabNavigation
